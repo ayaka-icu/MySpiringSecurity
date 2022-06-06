34b3d1dc5015d24dd9b9aaca21d3c3623c2a6f7c
尴尬酱 <2282514478@qq.com>
2022-06-06 21:13:07 +0800

    SpringSecurity学习 //_// day01/07 加油！

---
+++ SpringSecurity-02-Login/src/main/java/com/ganga/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ganga.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello SpringSecurity...");
+        return "Hello SpringSecurity...";
+    }
+}
