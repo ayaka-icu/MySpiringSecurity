5f2a5076e02120a96e74e72459c5315d8468b61c
尴尬酱 <2282514478@qq.com>
2022-06-14 08:27:33 +0800

    SpringSecurity学习 //_// day05/07

---
+++ SpringSecurity-09-CSRF-Mock/SpringSecurity-CSRF-Attack/src/main/java/com/ganga/CsrfAttackApplication.java
@@ -0,0 +1,13 @@
+package com.ganga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfAttackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsrfAttackApplication.class, args);
+    }
+
+}
