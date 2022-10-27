38881be2fdedb74b58d2a47cdd4e12742f013814
尴尬酱 <2282514478@qq.com>
2022-10-27 22:50:13 +0800

    SpringSecurity学习 //_// day06/07 修改

--- SpringSecurity-20-综合案例/SpringSecurity-Web/src/main/java/com/ganga/security/UserDetailsServiceImpl.java
+++ SpringSecurity-20-综合案例/SpringSecurity-Web/src/main/java/com/ganga/security/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.ganga.domain.Role;
 import com.ganga.domain.User;
 import com.ganga.mapper.RoleMapper;
-import com.ganga.mapper.UserMapper;
 import com.ganga.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,10 +21,11 @@ import java.util.List;
  * 实现密码自定更新加密
  */
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService , UserDetailsPasswordService {
+public class UserDetailsServiceImpl implements UserDetailsService, UserDetailsPasswordService {
 
     private final UserService userService;
     private final RoleMapper roleMapper;
+
     @Autowired
     public UserDetailsServiceImpl(UserService userService, RoleMapper roleMapper) {
         this.userService = userService;
@@ -37,7 +37,7 @@ public class UserDetailsServiceImpl implements UserDetailsService , UserDetailsP
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //用户输入的用户名 查询数据库
         LambdaQueryWrapper<User> qw = new LambdaQueryWrapper<>();
-        qw.eq(User::getUsername,username);
+        qw.eq(User::getUsername, username);
         User user = userService.getOne(qw);
         if (ObjectUtils.isEmpty(user)) throw new UsernameNotFoundException("用户名或密码错误");
         //查询当前用户的授权信息 并 将授权信息放入用户
@@ -50,6 +50,7 @@ public class UserDetailsServiceImpl implements UserDetailsService , UserDetailsP
 
     /**
      * 实现密码 自动更新 和 加密
+     *
      * @param user
      * @param newPassword
      * @return
@@ -58,11 +59,11 @@ public class UserDetailsServiceImpl implements UserDetailsService , UserDetailsP
     public UserDetails updatePassword(UserDetails user, String newPassword) {
         //更新数据库中的密码
         LambdaUpdateWrapper<User> uw = new LambdaUpdateWrapper<>();
-        uw.eq(User::getUsername,user.getUsername()) //eq 条件
-          .set(User::getPassword,newPassword);
+        uw.eq(User::getUsername, user.getUsername()) //eq 条件
+                .set(User::getPassword, newPassword);
         boolean isOK = userService.update(uw);
         //更新 UserDetails 认证对象
-        if (isOK){
+        if (isOK) {
             //由于user -> 接口是 UserDetails 类型 转成实现的User类型
             ((User) user).setPassword(newPassword);
         }
